efcb82a6d2bae9785dbacc136cd1a47d25d83b7b
Thilankavishka <thilankavishka25@gmail.com>
2024-11-21 20:52:09 +0530

    Add css and  update component details

--- Assignment 4/project/src/components/Gamecom.js
+++ Assignment 4/project/src/components/Gamecom.js
@@ -26,28 +26,35 @@ export default function Gamecom() {
   return (
     <>
       <div className="game-page">
-        <h1>Animal Matching Game</h1>
-        <div className="game-header">
-          <h2>Find: {targetAnimal.name}</h2>
+        <div>
+          <h1>Animal Matching Game</h1>
         </div>
-        <div className="animal-grid">
-          {animals.map((animal) => (
-            <div
-              key={animal.id}
-              className="animal-card"
-              onClick={() => handleAnimalClick(animal.id)}
-            >
-              <img src={animal.img} alt={animal.name} />
-              <p>{animal.name}</p>
-            </div>
-          ))}
-        </div>
-        {result && (
-          <div className="result">
-            <h2>{result}</h2>
-            <button onClick={restartGame}>Play Again</button>
+        <div>
+          <div>
+            {result && (
+              <div className="result">
+                <h2>Result</h2>
+                <h2>{result}</h2>
+                <button onClick={restartGame}>Play Again</button>
+              </div>
+            )}
           </div>
-        )}
+
+          <div className="game-header">
+            <h2>Animal Name: {targetAnimal.name}</h2>
+          </div>
+          <div className="animal-grid">
+            {animals.map((animal) => (
+              <div
+                key={animal.id}
+                className="animal-card"
+                onClick={() => handleAnimalClick(animal.id)}
+              >
+                <img src={animal.img} alt={animal.name} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
