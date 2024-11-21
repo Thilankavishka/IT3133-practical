97ba40d4a81e8eaef89520962a8ab125bc9b6f38
Thilankavishka <thilankavishka25@gmail.com>
2024-11-21 21:19:58 +0530

    Create Receive name function  and set this to Child Component

--- Day07_Practical/src/App.js
+++ Day07_Practical/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import ChildCom from "./components/ChildCom";
 
 function App() {
-  return <></>;
+  const [childname, setChildname] = useState("Default");
+  const receiveName = (name) => {
+    setChildname(name);
+  };
+  return (
+    <>
+      <div className="App">
+        <h2>Hello React I'm Parent</h2>
+        <ChildCom setfun={receiveName} />
+        <p>Parent:My child said:{childname}</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- Day07_Practical/src/components/ChildCom.js
+++ Day07_Practical/src/components/ChildCom.js
@@ -1,3 +1,7 @@
-export default function ChildCom() {
-  return <></>;
+export default function Childcom(props) {
+  return (
+    <>
+      <h3>Hello React I'm Child</h3>
+    </>
+  );
 }
